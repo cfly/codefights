2f38ab4c2c011f999644d9a6a0d617a927bfa168
cfly <caofei.org@gmail.com>
2016-11-16 22:28:51 +0800

    Initial commit

---
+++ src/main/java/org/caofei/codefights/arcade/AddTwoDigits.java
@@ -0,0 +1,7 @@
+package org.caofei.codefights.arcade;
+
+public class AddTwoDigits {
+    int addTwoDigits(int n) {
+        return n / 10 + n % 10;
+    }
+}
---
+++ src/test/java/org/caofei/codefights/arcade/AddTwoDigitsTest.java
@@ -0,0 +1,17 @@
+package org.caofei.codefights.arcade;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddTwoDigitsTest {
+    @Test
+    public void addTwoDigits() throws Exception {
+        AddTwoDigits addTwoDigits = new AddTwoDigits();
+        assertEquals(11, addTwoDigits.addTwoDigits(29));
+        assertEquals(12, addTwoDigits.addTwoDigits(48));
+        assertEquals(1, addTwoDigits.addTwoDigits(10));
+        assertEquals(7, addTwoDigits.addTwoDigits(25));
+    }
+
+}
